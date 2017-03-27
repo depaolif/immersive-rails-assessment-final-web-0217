7fbb59a1eb7e6e6fa3ad59ef4b4e202a792c7eba
Frederico De Paoli <frederico.de.paoli@gmail.com>
2017-03-27 14:47:05 -0400

    finished

--- app/controllers/appearances_controller.rb
+++ app/controllers/appearances_controller.rb
@@ -2,6 +2,10 @@ class AppearancesController < ApplicationController
 
   def new
     @appearance = Appearance.new
+    if params[:episode_id]
+      @episode = Episode.find(params[:episode_id])
+      @appearance.episode = @episode
+    end
   end
 
   def create
@@ -9,14 +13,28 @@ class AppearancesController < ApplicationController
     if @appearance.save
       redirect_to @appearance.episode
     else
-      redirect_to new_appearance_path
+      redirect_to new_appearance_path, alert: "Something went wrong with booking this appearance."
+    end
+  end
+
+  def edit
+    @appearance = Appearance.find(params[:id])
+  end
+
+  def update
+    @appearance = Appearance.find(params[:id])
+    @appearance.update(appearance_params)
+    if @appearance.save
+      redirect_to @appearance.episode
+    else
+      redirect_to edit_appearance_path(@appearance), alert: "Please make sure the rating is between 1 and 5."
     end
   end
 
   private
 
   def appearance_params
-    params.require(:appearance).permit(:guest_id, :episode_id, :rating)
+    params.require(:appearance).permit(:guest_id, :episode_id, :user_id, :rating)
   end
 
 end
--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -1,5 +1,8 @@
 class Appearance < ApplicationRecord
   belongs_to :guest
   belongs_to :episode
-  
+  belongs_to :user
+  belongs_to :producer, :class_name => :User, :foreign_key => "user_id"
+
+  validates :rating, inclusion: {in: 1..5}
 end
